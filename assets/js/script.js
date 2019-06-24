7db8aa48c70841d43a8865eae7952c53022e0e20
fahmadrifki <fahmadrifki@gmail.com>
2019-06-24 22:05:28 +0700

    add js script for toggle hamburger css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ $(document).ready(function(){
 });
 
 new WOW().init();
+
+// Hamburger Mobile Menu
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function(e) {
+  $hamburger.toggleClass("is-active");
+  $('your div menu id/class').toggle();
+});
